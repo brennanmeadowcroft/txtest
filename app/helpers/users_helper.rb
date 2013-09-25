507302c499a6dfbffd9b29e8a8bafcf4fefa5613
Brennan Meadowcroft <brennan@brennanmeadowcroft.com>
2013-09-24 19:12:53 -0600

    Add gravatar to top menu

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,7 +1,8 @@
 module UsersHelper
-	def gravatar_for(user)
+	def gravatar_for(user, custom_class=nil)
 		gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
 		gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}"
-		image_tag(gravatar_url, alt: user.email, class: "gravatar")
+		custom_class.nil? ? css_class="gravatar" : css_class=custom_class
+		image_tag(gravatar_url, alt: user.email, class: css_class)
 	end
 end
