d1e743c8e919301c684ca6868efa1120d930c6e0
Brennan Meadowcroft <brennan@brennanmeadowcroft.com>
2013-09-25 22:24:24 -0600

    Add character counter on question and answer

---
+++ app/assets/javascripts/questions.js
@@ -0,0 +1,18 @@
+$(document).ready(function() {
+	$('#question_question').keyup(function() {
+		var question_length = 150-($(this).val().length);
+		var count_class = "positive-count"
+		if (question_length < 0) {
+			var count_class = "negative-count"
+		}
+		$('#question-count').html(question_length).addClass(count_class);
+	});
+	$('#question_correct_answer').keyup(function() {
+		var answer_length = 150-($(this).val().length);
+		var count_class = "positive-count"
+		if (answer_length < 0) {
+			var count_class = "negative-count"
+		}
+		$('#answer-count').html(answer_length).addClass(count_class);
+	});
+});
